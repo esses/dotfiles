bad15e7f9520645921ad5d551e2ed8a111b8a126
esses <contact@esses.us>
2016-08-07 21:03:36 -0700

    basic git-enabled prompt

---
+++ zsh/prompt.zsh
@@ -0,0 +1,58 @@
+#!/usr/bin/env zsh
+#local return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
+
+setopt promptsubst
+autoload colors && colors
+
+if (( $+commands[git] ))
+then
+  git="$commands[git]"
+else
+  git="/usr/bin/git"
+fi
+
+git_branch() {
+  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+}
+
+git_dirty() {
+  if $(! $git status -s &> /dev/null)
+  then
+    echo ""
+  else
+    if [[ $($git status --porcelain) == "" ]]
+    then
+      echo "(%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%})"
+    else
+      echo "(%{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%})"
+    fi
+  fi
+}
+
+git_prompt_info () {
+ ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+ echo "${ref#refs/heads/}"
+}
+
+unpushed () {
+  $git cherry -v @{upstream} 2>/dev/null
+}
+
+need_push () {
+  if [[ $(unpushed) == "" ]]
+  then
+    echo " "
+  else
+    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+  fi
+}
+
+directory_name() {
+  echo "%{$fg_bold[cyan]%}%~%{$reset_color%}"
+}
+
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n› '
+set_prompt () {
+  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+}
