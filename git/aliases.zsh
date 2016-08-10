e35a10babfde385acdeae7d2da1337e553c4e877
esses <contact@esses.us>
2016-08-09 21:31:16 -0700

    git stash alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,3 +5,4 @@ alias gd='git diff origin/master'
 alias gca='git commit -am'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gbl='git branch -a'
+alias gs='git stash'
